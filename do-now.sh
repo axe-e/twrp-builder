6ddd8d843d37278755165cc4f34d1bc61a4e66fe
ZyCromerZ <neetroid97@gmail.com>
2021-09-25 20:47:35 +0700

    update
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- do-now.sh
+++ do-now.sh
@@ -1,34 +1,50 @@
 dpkg --add-architecture i386
-apt-get update -y
-apt-get -y install flex bison ncurses-dev texinfo gcc gperf patch libtool automake g++ libncurses5-dev gawk expat libexpat1-dev python-all-dev binutils-dev libgcc1:i386 bc libgnutls28-dev libcap-dev autoconf autoconf-archive libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev autopoint gettext liblzma-dev libssl-dev libz-dev
+rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
+docker rmi `docker images -q`
+apt-get remove account-plugin-facebook account-plugin-flickr account-plugin-jabber account-plugin-salut account-plugin-twitter account-plugin-windows-live account-plugin-yahoo aisleriot brltty duplicity empathy empathy-common example-content gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice-avmedia-backend-gstreamer libreoffice-base-core libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gnome libreoffice-gtk libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-style-galaxy libreoffice-style-human libreoffice-writer libsane libsane-common mcp-account-manager-uoa python3-uno rhythmbox rhythmbox-plugins rhythmbox-plugin-zeitgeist sane-utils shotwell shotwell-common telepathy-gabble telepathy-haze telepathy-idle telepathy-indicator telepathy-logger telepathy-mission-control-5 telepathy-salut totem totem-common totem-plugins printer-driver-brlaser printer-driver-foo2zjs printer-driver-foo2zjs-common printer-driver-m2300w printer-driver-ptouch printer-driver-splix
+git config --global user.name "ZyCromerZ"
+git config --global user.email "neetroid97@gmail.com"         
+apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php* 
+apt-get clean 
+apt-get -qq update
+apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev repo rsync flex curl  bison aria2
 
 git config --global color.ui auto
 
 mkdir -p $(pwd)/reep
 PATH="$(pwd)/reep:${PATH}"
+MAINPATH="$(pwd)"
 curl https://storage.googleapis.com/git-repo-downloads/repo > $(pwd)/reep/repo
 chmod a+rx $(pwd)/reep/repo
 
 mkdir recovery && cd recovery
 
-repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11
+repo init git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11 --depth=1 --partial-clone --clone-filter=blob:limit=10M --groups=all,-notdefault,-device,-darwin,-x86,-mips
 
 repo sync -j$(nproc --all)
 
 git clone --depth=1 https://github.com/ZyCromerZ/twrp_redmi_begonia -b android-11.0 device/redmi/begonia
 
 export ALLOW_MISSING_DEPENDENCIES=true
+ALLOW_MISSING_DEPENDENCIES=true
 
 . build/envsetup.sh
 
 lunch twrp_begonia-eng
 
-make -j$(nproc --all) recoveryimage
+repo sync -j$(nproc --all)
+
+mka recoveryimage -j$(nproc --all)
 
 cd ..
+
+cd recovery/out/target/product/begonia
+zip -r9 TWRP-begonia-miui-12.5.zip recovery.img
+cd "$MAINPATH"
 
 if [ -f recovery/out/target/product/begonia/recovery.img ];then
     git clone https://${GIT_SECRET}@github.com/ZyCromerZ/uploader-kernel-private
     chmod +x $(pwd)/uploader-kernel-private/run.sh
     . $(pwd)/uploader-kernel-private/run.sh "$(pwd)/recovery/out/target/product/begonia/recovery.img" "shared-file" "$(date +"%Y-%m-%d")"
+    . $(pwd)/uploader-kernel-private/run.sh "$(pwd)/recovery/out/target/product/begonia/TWRP-begonia-miui-12.5.zip" "shared-file" "$(date +"%Y-%m-%d")"
 fi
